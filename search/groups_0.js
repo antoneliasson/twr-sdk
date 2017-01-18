6b067be4e2839f34b9d8e1771a7cd631691e7bc5
mhubacek <mhubacek@cominfo.cz>
2017-01-18 14:12:14 +0100

    Deploy Doxygen content to GitHub Pages
        Travis CI build number: 46
        Content generated from: 6b3444b83d0c825e340d62f008e9838c21234f55

--- search/groups_0.js
+++ search/groups_0.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['bc_5fadc',['bc_adc',['../group__bc__adc.html',1,'']]],
   ['bc_5fdac',['bc_dac',['../group__bc__dac.html',1,'']]],
+  ['bc_5fdfu',['bc_dfu',['../group__bc__dfu.html',1,'']]],
   ['bc_5feeprom',['bc_eeprom',['../group__bc__eeprom.html',1,'']]],
   ['bc_5ffifo',['bc_fifo',['../group__bc__fifo.html',1,'']]],
   ['bc_5fgpio',['bc_gpio',['../group__bc__gpio.html',1,'']]],
