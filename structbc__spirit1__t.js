ff5f0c1e2b23b7a13c5997f38a66a9aa84e6d95e
Pavel Hübner <pavel.hubner@bigclown.com>
2017-01-25 19:04:34 +0100

    Deploy Doxygen content to GitHub Pages
        Travis CI build number: 24
        Content generated from: 1a29b8dfb249aceab656749e41a4e7d083994e6a

--- structbc__spirit1__t.js
+++ structbc__spirit1__t.js
@@ -3,9 +3,10 @@ var structbc__spirit1__t =
     [ "current_state", "structbc__spirit1__t.html#a2841286a0382909b4e2a3a733ec028ee", null ],
     [ "desired_state", "structbc__spirit1__t.html#a14979272d25b18990c30d6c394b5de83", null ],
     [ "event_handler", "structbc__spirit1__t.html#a36f9d0c11b7336d1ad70631626648e58", null ],
-    [ "rx_buffer", "structbc__spirit1__t.html#aa48fb8c93128bccca88afe97ff65a594", null ],
-    [ "rx_length", "structbc__spirit1__t.html#a83be6c93d7f9e1337bd170be63f422d1", null ],
+    [ "rx_buffer", "structbc__spirit1__t.html#ac258f6111704ca417c83bf05c1389ec4", null ],
+    [ "rx_length", "structbc__spirit1__t.html#a12e52263caa477fc4c54d684f27496a1", null ],
     [ "rx_tick_timeout", "structbc__spirit1__t.html#a1750cb0fc895de283ff5416dcda3b722", null ],
+    [ "rx_timeout", "structbc__spirit1__t.html#abdb4dfb8c4f52f6383bfcf136a2fe297", null ],
     [ "task_id", "structbc__spirit1__t.html#ab1ad1f1a362e1655cd8edc8790680109", null ],
     [ "tx_buffer", "structbc__spirit1__t.html#a58956201114431ddd49e77a0b9a111f7", null ],
     [ "tx_length", "structbc__spirit1__t.html#a189627160e3a2f509d38b3a5cd952ccc", null ]
