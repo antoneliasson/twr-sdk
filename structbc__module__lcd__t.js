af22eb3e18a4b9b181e923a604fc590653741735
Karel Blavka <karel.blavka@bigclown.com>
2017-03-08 13:42:43 +0000

    Deploy Doxygen content to GitHub Pages
        Travis CI build number: 140
        Content generated from: bbda37e83900e36f371c55f0116195260c9af0c8

--- structbc__module__lcd__t.js
+++ structbc__module__lcd__t.js
@@ -5,5 +5,6 @@ var structbc__module__lcd__t =
     [ "font", "structbc__module__lcd__t.html#a0ce63bccbef56e3fd4c7f10f11487971", null ],
     [ "framebuffer", "structbc__module__lcd__t.html#a6feb1707e8cc1d61cb3758942464432c", null ],
     [ "gpio", "structbc__module__lcd__t.html#a4738d1694900646dd72d607221f4696f", null ],
+    [ "rotation", "structbc__module__lcd__t.html#a4b70270a0321a71dcf0481734eefbe0d", null ],
     [ "tca9534a", "structbc__module__lcd__t.html#a651d5c51e1aa783919c6cfcbb98f36df", null ]
 ];
